01383cc7e38e333386a73d2bf0f74a432628c235
FelipeSalaa <47394517@est.ort.edu.ar>
2024-05-24 10:49:02 -0300

    coscu

--- Tp_pantallas/lib/core/app_router.dart
+++ Tp_pantallas/lib/core/app_router.dart
@@ -1,5 +1,7 @@
 import 'package:clase18_4/presentation/Screens/home_screen.dart';
 import 'package:clase18_4/presentation/Screens/Login_screen.dart';
+import 'package:clase18_4/presentation/Screens/register_screen.dart';
+
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
@@ -7,12 +9,18 @@ final appRouter = GoRouter(
   routes: [
     GoRoute(
       path: '/login',
+      name: LoginScreen.name,
       builder: (context, state) => LoginScreen(),
     ),
     GoRoute(
       path: '/home',
       name: HomeScreen.name,
       builder: (context, state) => HomeScreen(userName: state.extra as String),
+    ),
+    GoRoute(
+      path: '/register',
+      name: RegisterScreen.name,
+      builder: (context, state) => RegisterScreen(),
     ),
   ],
 );
--- Tp_pantallas/lib/presentation/Screens/login_screen.dart
+++ Tp_pantallas/lib/presentation/Screens/login_screen.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import 'package:clase18_4/presentation/Screens/home_screen.dart';
+import 'package:clase18_4/presentation/Screens/register_screen.dart';
 
 TextEditingController passController = TextEditingController();
 TextEditingController userController = TextEditingController();
 List<String> users = ['user', 'macaqunho', 'Papanatas'];
 List<String> pass = ['pass', 'dobrasil', 'agusminion04'];
 
+const String name = 'login';
+
 // ignore: must_be_immutable
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  
+
+  String userName;
+  LoginScreen({super.key, this.userName = ""});
 
   @override
   Widget build(BuildContext context) {
@@ -47,7 +53,8 @@ class LoginScreen extends StatelessWidget {
                 );
                 ScaffoldMessenger.of(context).showSnackBar(snackBar);
                 context.pushNamed(HomeScreen.name, extra: users[inputUser]);
-              } else if (userController.text == '' || passController.text == '') {
+              } else if (userController.text == '' ||
+                  passController.text == '') {
                 SnackBar snackBar = const SnackBar(
                   content: Text('Campo Vacío'),
                   duration: Duration(milliseconds: 1500),
@@ -64,6 +71,13 @@ class LoginScreen extends StatelessWidget {
               }
             },
             child: const Text('Iniciar sesión'),
+          ),
+          const SizedBox(height: 50),
+          ElevatedButton(
+            onPressed: () {
+              context.pushNamed(RegisterScreen.name);
+            },
+            child: const Text('Registrarse'),
           ),
         ],
       ),
