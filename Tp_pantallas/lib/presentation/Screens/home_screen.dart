a98b2c74e2c6f1b37b111b864b63be7391b2779a
FelipeSalaa <162701468+FelipeSalaa@users.noreply.github.com>
2024-05-30 11:41:59 -0300

    listas con materias y profes

--- Tp_pantallas/lib/presentation/Screens/home_screen.dart
+++ Tp_pantallas/lib/presentation/Screens/home_screen.dart
@@ -2,6 +2,7 @@
 import 'package:clase18_4/presentation/Screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+
 // ignore: must_be_immutable
 class HomeScreen extends StatelessWidget {
   static const String name = 'home';
@@ -11,9 +12,38 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    List<String> titulos = ["primero", "segundo", "tercero"];
-    List<String> descripcion = ["1", "2", "3"];
+    List<String> titulos = [
+      "Cultura Judia lii",
+      "Desarrollo de aplicaciones informaticas ",
+      "Educación Fisica",
+      "Educación Judia",
+      "Estructura y Funcionamiento de los Sistemas li ",
+      "Filosofia",
+      "Historia",
+      "Inglés"
+      "Lengua y Literatura",
+      "Matematica",
+      "Metodología de Proyectos",
+      "Proyectos de Producción",
+      "Quimica",
+      "Seminario de Informatica y Telecomunicaciones"
+    ];
+    List<String> descripcion = [
+      "Ivon Lorena Steiner",
+      "Alejandro Breitman",
+      "Mariano Gonzalez Slaibe",
+      "Demian Jmelnizky",
+      "Marcos Alegre",
+      "Daniela Alejandra Bustamante",
+      "Mirko Veckiardo",
+      "Profesor correspondiente a cada nivel",
+      "Maria del Pilar Lopez Vera",
+      "Sofía Romano",
+      "Alejandro Breitman",
+      "Eduardo Brandes",
+      "Pablo Stark",
+      "Mirko Veckiardo"
+    ];
 
     return Scaffold(
       body: ListView.builder(
