d147aebb186cd50f50b8371c780005031f885031
FelipeSalaa <162701468+FelipeSalaa@users.noreply.github.com>
2024-07-04 12:15:57 -0300

    Agora si Manin

--- Tp_pantallas/lib/core/entities.dart
+++ Tp_pantallas/lib/core/entities.dart
@@ -1,42 +1,13 @@
 class Materia {
-  var name;
-  var description;
-  var img;
-}
+  final int id;
+  final String name;
+  final String description;
+ // final String image;
 
-    Materia Matematica = Materia()
-    ..name = "Matematica"
-    ..description = "Sofia Roma";
-    Materia Lengua = Materia()
-    ..name = "Lengua"
-    ..description = "Piliiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii";
-    Materia Dap1 = Materia()
-    ..name = "Dap1"
-    ..description = "Ale";
-    Materia Dap2 = Materia()
-    ..name = "Dap2"
-    ..description = "Ruben";
-    Materia HRC = Materia()
-    ..name = "HRC"
-    ..description = "Mirko & Prieto";
-    Materia ST = Materia()
-    ..name = "ST"
-    ..description = "Mirko & Prieto";
-    Materia CultJudia = Materia()
-    ..name = "Cultura Judia"
-    ..description = "Meir";
-    Materia EdJudia = Materia()
-    ..name = "Educacion Judia"
-    ..description = "Demian";
-    Materia EdFisica = Materia()
-    ..name = "Educación Física"
-    ..description = "Mariano Moreno";
-    Materia Quimica = Materia()
-    ..name = "Quimica"
-    ..description = "Pablito Sr. Stark";
-    Materia Filosofia = Materia()
-    ..name = "Filosofia"
-    ..description = "Daniela Bustamante";
-    Materia PP = Materia()
-    ..name = "Producción De Proyectos"
-    ..description = "Facufablab & Edu";
+  Materia({
+    required this.id,
+    required this.name,
+    required this.description,
+   // required this.image,
+  });
+}
--- Tp_pantallas/lib/presentation/Screens/home_screen.dart
+++ Tp_pantallas/lib/presentation/Screens/home_screen.dart
@@ -2,7 +2,10 @@
 import 'package:clase18_4/presentation/Screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:clase18_4/core/entities.dart';
+import 'package:clase18_4/core/data.dart';
 
+List<Materia> materia  = materias; // <obj> nombre = lista
 // ignore: must_be_immutable
 class HomeScreen extends StatelessWidget {
   static const String name = 'home';
@@ -12,43 +15,31 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Object> Materias = [
-      Matematica,
-      Lengua,
-      Quimica,
-      Filosofia,
-      EdFisica,
-      EdJudia,
-      CultJudia,
-      Dap1,
-      Dap2,
-      HRC,
-      ST,
-      PP
-    ];
 
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 0, 0, 0),
       appBar: AppBar(
         title: const Text("Materias 5to año"),
         centerTitle: true,
-        backgroundColor: const Color.fromARGB(86, 0, 230, 54),
+        backgroundColor: Color.fromARGB(189, 0, 243, 57),
       ),
       body: Center(
         child: Container(
           width: 500,
           height: double.infinity,
-          color: const Color.fromARGB(37, 60, 57, 46),
+          color: Color.fromARGB(122, 0, 0, 76),
           margin: const EdgeInsets.all(20),
           padding: const EdgeInsets.all(30),
           child: ListView.separated(
-            itemCount: Materias.length,
+            itemCount: materias.length,
             itemBuilder: (context, index) {
+              final materia = materias[index];
               return Card(
+                
+                color: Color.fromARGB(255, 2, 221, 255),
                 child: ListTile(
-                  title: Text(
-                    Materias[index],
-                  ),
-                  subtitle: Text(Materias.description[index]),
+                  title: Text(materia.name),
+                  subtitle: Text(materia.description),
                 ),
               );
             },
